457f798a0ce853f891337121af6ee1d579905648
KImmelid <santirestrepo.osorio@gmail.com>
2026-04-09 13:08:46 -0500

    CHG: Fix Client hierarchy separating natural person properties

--- demo/src/main/java/app/domain/models/PersonClient.java
+++ demo/src/main/java/app/domain/models/PersonClient.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public abstract class Client extends Person {
-    private ArrayList<BankAccount> bankAccounts;
+public class PersonClient extends Client {
+
+    private LocalDate birthDate;
 }
