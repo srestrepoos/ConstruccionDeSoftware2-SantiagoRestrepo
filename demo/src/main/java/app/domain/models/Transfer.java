c950a24dc5617be76151457fb22f976b2ebb89f1
KImmelid <santirestrepo.osorio@gmail.com>
2026-04-09 13:08:47 -0500

    CHG: Migrate Transfer to rich domain adding execute method and precise types

--- demo/src/main/java/app/domain/models/Transfer.java
+++ demo/src/main/java/app/domain/models/Transfer.java
@@ -4,20 +4,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Timestamp;
-
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
 public class Transfer {
 
     private long transferId;
-    private String sourceAccount;
-    private String targetAccount;
-    private double amount;
-    private Timestamp creationDate;
-    private Timestamp approvalDate;
+    private BankAccount sourceAccount;
+    private BankAccount targetAccount;
+    private BigDecimal amount;
+    private LocalDateTime creationDate;
+    private LocalDateTime approvalDate;
     private TransferStatus transferStatus;
     private long creatorUserId;
     private Long approverUserId;
+
+    public void execute() {
+        if (this.transferStatus == TransferStatus.APPROVED && this.sourceAccount != null && this.targetAccount != null) {
+            this.sourceAccount.withdraw(this.amount);
+            this.targetAccount.deposit(this.amount);
+            this.transferStatus = TransferStatus.EXECUTED;
+        }
+    }
 }
